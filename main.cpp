8c652ebbb57a409f6f54b420cea29b1e952d8e53
Kayle54187 <habichristi@gmail.com>
2024-01-19 23:34:12 +0200

    feat: Checking Collision with Food

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <raylib.h>
 #include <deque>
+#include <raymath.h>
 
 using namespace std;
 
@@ -14,16 +15,34 @@ Color darkGreen = {43, 51, 24, 255};
 int cellSize = 30;
 int cellCount = 25;
 
+double lastUpdateTime = 0;
+
+bool eventTriggered(double interval) {
+    double currentTime = GetTime();
+    if (currentTime - lastUpdateTime >= interval) {
+        lastUpdateTime = currentTime;
+        return true;
+    }
+    return false;
+}
+
 class Snake {
 public:
     deque<Vector2> body = {Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9}};
+    Vector2 direction = {1, 0};
 
     void Draw() {
-        for(int i = 0; i < body.size(); i++){
-            int x = body[i].x;
-            int y = body[i].y;
-            DrawRectangle(x*cellSize, y*cellSize, cellSize, cellSize, darkGreen);
+        for (unsigned i = 0; i < body.size(); i++) {
+            float x = body[i].x;
+            float y = body[i].y;
+            Rectangle segment = Rectangle{x * cellSize, y * cellSize, (float) cellSize, (float) cellSize};
+            DrawRectangleRounded(segment, 0.5, 6, darkGreen);
         }
+    }
+
+    void Update() {
+        body.pop_back();
+        body.push_front(Vector2Add(body[0], direction));
     }
 };
 
@@ -54,23 +73,51 @@ public:
     }
 };
 
+class Game {
+public:
+    Snake snake = Snake();
+    Food food = Food();
+
+    void Draw() {
+        food.Draw();
+        snake.Draw();
+    }
+
+    void Update() {
+        snake.Update();
+        CheckCollisionWithFood();
+    }
+
+    void CheckCollisionWithFood() {
+        if(Vector2Equals(snake.body[0], food.position)){
+            food.position = food.GenerateRandomPosition();
+        }
+    }
+};
+
 
 int main() {
 
     cout << "Starting Game" << endl;
 
     InitWindow(cellSize * cellCount, cellSize * cellCount, "Retro Snake");
+    SetTargetFPS(60);
 
-    Food food = Food();
-    Snake snake = Snake();
+    Game game = Game();
 
     while (!WindowShouldClose()) {
         BeginDrawing();
 
+        if (eventTriggered(0.2)) game.Update();
+
+        if (IsKeyPressed(KEY_UP) && game.snake.direction.y != 1) game.snake.direction = {0, -1};
+        if (IsKeyPressed((KEY_DOWN)) && game.snake.direction.y != -1) game.snake.direction = {0, 1};
+        if (IsKeyPressed((KEY_LEFT)) && game.snake.direction.x != 1) game.snake.direction = {-1, 0};
+        if (IsKeyPressed(KEY_RIGHT) && game.snake.direction.x != -1) game.snake.direction = {1, 0};
+
         //Drawing
         ClearBackground(green);
-        food.Draw();
-        snake.Draw();
+        game.Draw();
 
         EndDrawing();
     }
