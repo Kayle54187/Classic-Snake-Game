ed2c34d396c1ea194872c684a60566628dc2986c
Kayle54187 <habichristi@gmail.com>
2024-01-19 22:55:59 +0200

    feat: Adding Snake Class and Draw method

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <raylib.h>
+#include <deque>
 
 using namespace std;
 
@@ -13,28 +14,42 @@ Color darkGreen = {43, 51, 24, 255};
 int cellSize = 30;
 int cellCount = 25;
 
+class Snake {
+public:
+    deque<Vector2> body = {Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9}};
+
+    void Draw() {
+        for(int i = 0; i < body.size(); i++){
+            int x = body[i].x;
+            int y = body[i].y;
+            DrawRectangle(x*cellSize, y*cellSize, cellSize, cellSize, darkGreen);
+        }
+    }
+};
+
 class Food {
 public:
     Vector2 position;
     Texture2D texture;
 
-    Food(){
+    Food() {
         Image image = LoadImage("Graphics/food.png");
         texture = LoadTextureFromImage(image);
         position = GenerateRandomPosition();
         UnloadImage(image);
     }
-    ~Food(){
+
+    ~Food() {
         UnloadTexture(texture);
     }
 
     void Draw() {
-        DrawTexture(texture,position.x * cellSize, position.y * cellSize, WHITE);
+        DrawTexture(texture, position.x * cellSize, position.y * cellSize, WHITE);
     }
 
     Vector2 GenerateRandomPosition() {
-        float x = GetRandomValue(0, cellCount-1);
-        float y = GetRandomValue(0, cellCount-1);
+        float x = GetRandomValue(0, cellCount - 1);
+        float y = GetRandomValue(0, cellCount - 1);
         return Vector2{x, y};
     }
 };
@@ -47,6 +62,7 @@ int main() {
     InitWindow(cellSize * cellCount, cellSize * cellCount, "Retro Snake");
 
     Food food = Food();
+    Snake snake = Snake();
 
     while (!WindowShouldClose()) {
         BeginDrawing();
@@ -54,6 +70,7 @@ int main() {
         //Drawing
         ClearBackground(green);
         food.Draw();
+        snake.Draw();
 
         EndDrawing();
     }
